6c1d8065fb27025ac4173ff1c7a7c85aeb619928
Bharti Sharma <sharma.bharti2608@gmail.com>
2021-06-09 01:57:19 +0530

    adding browser router

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
@@ -13,21 +12,3 @@ ReactDOM.render(
 );
 
 reportWebVitals();
-=======
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.js';
-import {BrowserRouter} from 'react-router-dom';
-import App from './App';
-import './assets/sass/index.scss';
-ReactDOM.render(
-    <BrowserRouter>
-    <App/>
-    </BrowserRouter>,
-  document.getElementById('root')
-);
-
-
-
->>>>>>> cafe14b6bfff971999d9d1daff354a60190214d9
